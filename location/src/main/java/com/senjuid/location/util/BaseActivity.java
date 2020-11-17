d73b715e1b9edb8d3dede829abe5b857e5280629
bayu iman <senjuid@gmail.com>
2020-11-17 09:13:15 +0700

    - fixing bugs language on androidx - menambahkan language malaysia

--- location/src/main/java/com/senjuid/location/util/BaseActivity.java
+++ location/src/main/java/com/senjuid/location/util/BaseActivity.java
@@ -1,11 +1,23 @@
 package com.senjuid.location.util;
 
 import android.content.Context;
+import android.content.res.Configuration;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(LocaleHelper.onAttach(newBase, LocaleHelper.getLanguage(newBase)));
+    }
+
+    @Override
+    public void applyOverrideConfiguration(Configuration overrideConfiguration) {
+        if (overrideConfiguration != null) {
+            int uiMode = overrideConfiguration.uiMode;
+            overrideConfiguration.setTo(getBaseContext().getResources().getConfiguration());
+            overrideConfiguration.uiMode = uiMode;
+        }
+        super.applyOverrideConfiguration(overrideConfiguration);
     }
 }
